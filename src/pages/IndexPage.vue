cd802494332d2aa22856bfe496c9d426dd58601f
Rogerio Dias <rogeriomanueldias@gmail.com>
2022-09-15 09:59:34 +0100

    Created the login page's layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,52 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="column items-center justify-center q-gutter-y-md">
+    <q-img src="icons/favicon-128x128.png" style="max-width: 100px" />
+    <span class="text-center" style="max-width: 250px">{{
+      t('login.header')
+    }}</span>
+    <q-card>
+      <q-card-section>
+        <q-input :label="t('login.email')" v-model="email" type="email" />
+        <q-input
+          :label="t('login.password')"
+          v-model="password"
+          :type="isPwd ? 'password' : 'text'"
+          :rules="[
+            (v) => v.length >= 8 || 'Password must be 8 digits or longer',
+          ]"
+          lazy-rules
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+      </q-card-section>
+      <q-card-section class="q-gutter-x-md q-mt-lg">
+        <q-btn :label="t('login.login')" color="primary" @click="login" />
+        <q-btn
+          :label="t('login.createAccount')"
+          color="primary"
+          to="/register"
+        />
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+const { t } = useI18n();
+const email = ref('');
+const password = ref('');
+const isPwd = ref(true);
+
+function login() {
+  alert('método');
+}
 </script>
