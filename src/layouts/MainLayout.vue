de402c28ea28ddaa8dbbb08a2fa87a4de7af4d5c
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2022-09-30 18:41:58 +0100

    Begun changing the main layout to have tabs navigation

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,9 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header reveal bordered class="bg-primary text-white">
+    <q-header reveal class="bg-secondary text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-        <q-toolbar-title class="row justify-center">
-          <q-btn dense flat round to="/overview">
-            <q-avatar>
-              <img src="icons/logo_white.svg" />
-            </q-avatar>
-          </q-btn>
-        </q-toolbar-title>
+        <q-toolbar-title> Overview </q-toolbar-title>
 
         <q-btn dense flat round icon="account_circle">
           <q-popup-proxy side="right" style="width: 200px">
@@ -29,7 +23,13 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" elevated>
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      elevated
+      outline
+    >
       <q-list>
         <sidebar-item
           v-for="(page, index) in pagesLeft"
@@ -50,13 +50,19 @@
         <router-view />
       </q-page>
     </q-page-container>
+    <q-footer class="bg-dark" bordered>
+      <q-tabs>
+        <q-route-tab icon="home" to="/home" label="Home" />
+        <q-route-tab icon="swap_vert" to="/transfers" label="Transfers" />
+        <q-route-tab icon="credit_card" to="/cards" label="Cards" />
+        <q-route-tab icon="person" to="/account" label="Account" />
+      </q-tabs>
+    </q-footer>
   </q-layout>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import CollapsibleSidebarItem from 'src/components/CollapsibleSidebarItem.vue';
-import LinkSidebarItem from 'src/components/LinkSidebarItem.vue';
 import SidebarPage from 'src/types/SidebarPage';
 import SidebarItem from 'components/SidebarItem.vue';
 
