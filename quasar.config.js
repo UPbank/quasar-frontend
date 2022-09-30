38348d474c94703fc4754b6f221a37e76973278a
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2022-09-30 18:41:23 +0100

    Added dark theme and changed theme colors

--- quasar.config.js
+++ quasar.config.js
@@ -97,7 +97,12 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config: {
+        dark: true,
+        notify: {
+          position: 'top',
+        },
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
