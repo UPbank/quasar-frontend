4c4bad35cb68fa81a38b1cd23b52e94ddd4cdcbf
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2022-10-01 13:36:39 +0100

    Put all elements into one parent element

--- src/pages/OverView.vue
+++ src/pages/OverView.vue
@@ -1,117 +1,119 @@
 <template>
-  <q-card>
-    <q-card-section class="column items-center">
-      <div>Balance</div>
-      <div v-if="account" class="text-h5">
-        {{ (account.balance / 100).toFixed(2) }} €
-      </div>
-      <q-spinner v-else size="lg" class="q-mt-md" />
-    </q-card-section>
-  </q-card>
-  <p></p>
   <div>
-    <q-card class="row justify-evenly">
-      <q-select
-        item-aligned
-        style="max-width: 300px"
-        v-model="typeFilter"
-        :options="options"
-        emit-value
-        map-options
+    <q-card>
+      <q-card-section class="column items-center">
+        <div>Balance</div>
+        <div v-if="account" class="text-h5">
+          {{ (account.balance / 100).toFixed(2) }} €
+        </div>
+        <q-spinner v-else size="lg" class="q-mt-md" />
+      </q-card-section>
+    </q-card>
+    <p></p>
+    <div>
+      <q-card class="row justify-evenly">
+        <q-select
+          item-aligned
+          style="max-width: 300px"
+          v-model="typeFilter"
+          :options="options"
+          emit-value
+          map-options
+        />
+        <q-input
+          style="width: 300px"
+          class="col self-center"
+          v-model="filter"
+          placeholder="Search"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </q-card>
+
+      <q-card class="my-card full-width">
+        <q-card-section>
+          <div class="text-h6">Filter by Date</div>
+        </q-card-section>
+        <q-card-section class="row">
+          <div class="q-pa-md" style="max-width: 300px">
+            <q-input filled v-model="startDate" mask="date" :rules="['date']">
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-date v-model="startDate">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          :label="t('Close')"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+          <div class="q-pa-md" style="max-width: 300px">
+            <q-input filled v-model="startDate" mask="date" :rules="['date']">
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-date v-model="startDate">
+                      <div class="row items-center justify-end">
+                        <q-btn
+                          v-close-popup
+                          :label="t('Close')"
+                          color="primary"
+                          flat
+                        />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
+    <!-- <span class="text-caption full-width" style="max-width: 401px"> 30 ago</span> -->
+
+    <q-infinite-scroll
+      @load="onLoad"
+      :offset="250"
+      :disable="!hasMorePages"
+      :initial-index="-1"
+      :key="queryString"
+    >
+      <transfer-item
+        v-for="transaction in transactions"
+        :name="transaction.sender"
+        :key="transaction.id"
+        :amount="`${transaction.amount > 0 ? '+' : ''}${(
+          transaction.amount / 100
+        ).toFixed(2)} €`"
+        :time="transaction.time"
       />
-      <q-input
-        style="width: 300px"
-        class="col self-center"
-        v-model="filter"
-        placeholder="Search"
-      >
-        <template v-slot:append>
-          <q-icon name="search" />
-        </template>
-      </q-input>
-    </q-card>
-
-    <q-card class="my-card full-width">
-      <q-card-section>
-        <div class="text-h6">Filter by Date</div>
-      </q-card-section>
-      <q-card-section class="row">
-        <div class="q-pa-md" style="max-width: 300px">
-          <q-input filled v-model="startDate" mask="date" :rules="['date']">
-            <template v-slot:append>
-              <q-icon name="event" class="cursor-pointer">
-                <q-popup-proxy
-                  cover
-                  transition-show="scale"
-                  transition-hide="scale"
-                >
-                  <q-date v-model="startDate">
-                    <div class="row items-center justify-end">
-                      <q-btn
-                        v-close-popup
-                        :label="t('Close')"
-                        color="primary"
-                        flat
-                      />
-                    </div>
-                  </q-date>
-                </q-popup-proxy>
-              </q-icon>
-            </template>
-          </q-input>
+      <template v-slot:loading>
+        <div class="row justify-center q-my-md">
+          <q-spinner-dots color="primary" size="40px" />
         </div>
-        <div class="q-pa-md" style="max-width: 300px">
-          <q-input filled v-model="startDate" mask="date" :rules="['date']">
-            <template v-slot:append>
-              <q-icon name="event" class="cursor-pointer">
-                <q-popup-proxy
-                  cover
-                  transition-show="scale"
-                  transition-hide="scale"
-                >
-                  <q-date v-model="startDate">
-                    <div class="row items-center justify-end">
-                      <q-btn
-                        v-close-popup
-                        :label="t('Close')"
-                        color="primary"
-                        flat
-                      />
-                    </div>
-                  </q-date>
-                </q-popup-proxy>
-              </q-icon>
-            </template>
-          </q-input>
-        </div>
-      </q-card-section>
-    </q-card>
+      </template>
+    </q-infinite-scroll>
+    <span v-if="!hasMorePages">No more transactions</span>
   </div>
-  <!-- <span class="text-caption full-width" style="max-width: 401px"> 30 ago</span> -->
-
-  <q-infinite-scroll
-    @load="onLoad"
-    :offset="250"
-    :disable="!hasMorePages"
-    :initial-index="-1"
-    :key="queryString"
-  >
-    <transfer-item
-      v-for="transaction in transactions"
-      :name="transaction.sender"
-      :key="transaction.id"
-      :amount="`${transaction.amount > 0 ? '+' : ''}${(
-        transaction.amount / 100
-      ).toFixed(2)} €`"
-      :time="transaction.time"
-    />
-    <template v-slot:loading>
-      <div class="row justify-center q-my-md">
-        <q-spinner-dots color="primary" size="40px" />
-      </div>
-    </template>
-  </q-infinite-scroll>
-  <span v-if="!hasMorePages">No more transactions</span>
 </template>
 
 <script setup lang="ts">
