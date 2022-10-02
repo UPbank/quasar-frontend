9490791431b8d81a9ef8bbeadaedbdbc6a498bbc
Pedro Cavalheiro <pedro.cavalheiro@isat.pt>
2022-10-02 16:44:53 +0100

    Merged with hasbacklayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,13 @@
 <template>
   <q-layout view="hHh lpR fFf">
+    <q-header class="bg-dark" v-if="false">
+      <q-toolbar>
+        <q-btn flat dense round icon="arrow_back" @click="$router.back()" />
+        <q-toolbar-title>
+          <span class="q-ml-sm">Transfer</span>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
     <q-page-container
       class="column items-center full-width"
       id="page-container"
@@ -53,22 +61,40 @@
     background: linear-gradient(
         35deg,
         rgba($accent, 0.2) 1%,
-        rgba(255, 255, 255, 0) 20%,
+        rgba(255, 255, 255, 0) 40%,
         #fff
       ),
       linear-gradient(
         to top,
         rgba($primary, 0.4) 1%,
-        rgba(255, 255, 255, 0) 40%,
+        rgba(255, 255, 255, 0) 80%,
         #fff
       ),
       linear-gradient(
           -35deg,
           rgba($secondary, 0.2) 1%,
-          rgba(255, 255, 255, 0) 20%,
+          rgba(255, 255, 255, 0) 40%,
           #fff
         )
         #fff;
+  }
+}
+
+.body--light,
+.body--dark {
+  #page-container {
+    background-size: 100% 200%;
+    animation: gradient 2s ease-out;
+    background-position: 0% 75%;
+  }
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 0%;
+  }
+  100% {
+    background-position: 0% 75%;
   }
 }
 </style>
